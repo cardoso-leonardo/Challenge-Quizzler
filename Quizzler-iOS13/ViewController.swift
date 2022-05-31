36823b6940cf8278d64667014b8597d63f2dffc5
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2022-05-31 00:16:49 -0300

    Implemented Quiz Functionality

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -5,16 +5,54 @@
 //  Created by Angela Yu on 12/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
+// MARK: Imports
 import UIKit
 
+
+
 class ViewController: UIViewController {
+//MARK: Declarations
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var questionNumber = 0
+    
+    var quiz : [[String]] = [
+        ["Leonardo is studying to become an ios developer","True"],
+        ["Leonardo likes to play soccer", "False"],
+        ["Leonardo is a big fan of Star Wars","True"]
+    ]
+    
+//MARK: Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
 
